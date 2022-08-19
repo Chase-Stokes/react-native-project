6cc6a807552151d27afdf00605835339b7c345b6
chase-stokes <chasehstokes@gmail.com>
2022-08-19 03:01:04 -0700

    practice with implementing styles

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Oh Snap!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Oh Snap!!!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>This is showing up on the emulator!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDD0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'walnut',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: 'aquamarine',
+    padding: 10
+  }
 });
